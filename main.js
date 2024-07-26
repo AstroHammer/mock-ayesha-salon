de15fe6d1732b9b207bcb0319d893f0f72a5ef21
Astro <jpalato101@gmail.com>
2024-07-26 16:20:36 -0700

    worked on service accordions, started FAQ section with mini accordions

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-{\rtf1}
+const accordionButtons = document.querySelectorAll('.accordion-item-btn');
+
+accordionButtons.forEach(accordionButton => {
+    accordionButton.addEventListener('click', event => {
+        accordionButton.classList.toggle('active');
+        const accordionItemBody = accordionButton.parentElement.nextElementSibling;
+        if (accordionButton.classList.contains('active')) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + 'px';
+        } else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+    })
+})
+
+const smallAccordionButtons = document.querySelectorAll('.single-cordion-item-btn');
+
+smallAccordionButtons.forEach(accordionButton => {
+    accordionButton.addEventListener('click', event => {
+        accordionButton.classList.toggle('active');
+        const accordionItemBody = accordionButton.parentElement.nextElementSibling;
+        if (accordionButton.classList.contains('active')) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + 'px';
+        } else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+    })
+})
+
+const accordionTitle =  document.querySelectorAll('.accordion-title');
+
