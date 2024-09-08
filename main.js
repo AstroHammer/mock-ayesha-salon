4372133391351930ff879acb580a215ca2d2fb5c
Pokedex <jpalato101@gmail.com>
2024-09-08 15:43:07 -0700

    testing mobile layout

--- main.js
+++ main.js
@@ -63,64 +63,87 @@ let scrollingGrid = document.querySelector('.flow-grid');
 scrollingGrid.addEventListener('wheel', (event) => {
     event.preventDefault();
     scrollingGrid.scrollBy({
-      left: event.deltaY < 0 ? -80 : 80,
+      left: event.deltaY < 0 ? -150 : 150,
     });
 
 
 });
 window.onload = function() {
-
     let item = document.querySelectorAll('.item');
-    item.forEach(div => {
-        let itemChilds = div.children;
+    
+    let slideContainer = document.querySelector('.contents-wrap');
+
+    createSlideObserver();
+    function createSlideObserver() {
+        let observerOne;
+        let optionsOne = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 1
+        };
+        observerOne = new IntersectionObserver(primeCollage, optionsOne);
+        observerOne.observe(slideContainer);
+    }
+    function primeCollage(entries, observer) {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                activateCollage();
+                observer.unobserve(slideContainer);
+            }
+        })
+    }
+
+
+    
+    function activateCollage() {
+        item.forEach(div => {
+            let itemChilds = div.children;
         
-
-        for (let i = 0; i < itemChilds.length; i++) {
-
-            let ogWidth = itemChilds[i].getBoundingClientRect().width;
-            let itemWidth = Math.ceil(ogWidth);
-            
-            setSlidePosition(itemChilds[i], itemWidth);
-            activateTranslate(itemChilds[i], div, itemWidth);
+            for (let i = 0; i < itemChilds.length; i++) {
+                let ogWidth = itemChilds[i].getBoundingClientRect().width;
+                let itemWidth = Math.ceil(ogWidth);
+        
+                setSlidePosition(itemChilds[i], itemWidth);
+                activateTranslate(itemChilds[i], div, itemWidth);
+            }
+        })
+        function setSlidePosition(itemChilds, itemWidth) {
+            itemChilds.style.transform = `translateX(${itemWidth}px)`;
         }
-    })
-    function setSlidePosition(itemChilds, itemWidth) {
-        itemChilds.style.transform = `translateX(${itemWidth}px)`;
-    }
-    function activateTranslate(itemChilds, div, itemWidth) {
-        createObserver();
-        function createObserver() {
-            let observer;
-            let options = {
-                root: null,
-                rootMargin: '0px',
-                threshold: .1,
-            };
-            observer = new IntersectionObserver(handleTranslate, options);
-            observer.observe(div);
-        }
-        function handleTranslate(entries, observer) {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    if (itemChilds.classList.contains('pre-slide-bg')) {
-                        activateFirstSlide(itemChilds, itemWidth);
-                        observer.unobserve(div);
-                    } else {
-                        activateSecondSlide(itemChilds, itemWidth);
-                        observer.unobserve(div);
+        function activateTranslate(itemChilds, div, itemWidth) {
+            createItemObserver();
+            function createItemObserver() {
+                let observerTwo;
+                let optionsTwo = {
+                    root: null,
+                    rootMargin: '0px',
+                    threshold: .1,
+                };
+                observerTwo = new IntersectionObserver(handleTranslate, optionsTwo);
+                observerTwo.observe(div);
+            }
+            function handleTranslate(entries, observer) {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        if (itemChilds.classList.contains('pre-slide-bg')) {
+                            activateFirstSlide(itemChilds, itemWidth);
+                            observer.unobserve(div);
+                        } else {
+                            activateSecondSlide(itemChilds, itemWidth);
+                            observer.unobserve(div);
+                        }
                     }
-                }
-            })
+                })
+            }
         }
-    }
-
-    function activateFirstSlide(itemChilds, itemWidth) {
-        itemChilds.style.transition = 'transform .8s cubic-bezier(.6,0,.3,1)';
-        itemChilds.style.transform = `translateX(${-itemWidth + itemWidth}px)`;
-    }
-    function activateSecondSlide(itemChilds, itemWidth) {
-        itemChilds.style.transition = 'transform .8s .8s cubic-bezier(.6,0,.3,1)';
-        itemChilds.style.transform = `translateX(${-itemWidth + itemWidth}px)`;
+        function activateFirstSlide(itemChilds, itemWidth) {
+            itemChilds.style.transition = 'transform .8s cubic-bezier(.6,0,.3,1)';
+            itemChilds.style.transform = `translateX(${-itemWidth + itemWidth}px)`;
+        }
+        function activateSecondSlide(itemChilds, itemWidth) {
+            itemChilds.style.transition = 'transform .8s .8s cubic-bezier(.6,0,.3,1)';
+            itemChilds.style.transform = `translateX(${-itemWidth + itemWidth}px)`;
+        }
     }
 
 }
