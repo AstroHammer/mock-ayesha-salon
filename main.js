5e3e6431c5c978d1c04f80a3ad522d24b5c1d637
Pokedex <jpalato101@gmail.com>
2024-09-09 09:16:09 -0700

    mobile nav

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+//navigation menu
+const primaryNav = document.querySelector('.mobile-nav');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+
+    if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } else if (visibility === "true") {
+        primaryNav.setAttribute('data-visible', false);
+        navToggle.setAttribute('aria-expanded', false);
+        primaryNav.style.transition = "transform .3s ease-in";
+    }
+});
+
+
+
+
+
+
 //Services Accordion Boxes
 
 const accordionButtons = document.querySelectorAll('.accordion-item-btn');
